785038c752e12b33b5bfdddc857ed5154a7192d8
Jacob Shtokolov <jacob.100205@gmail.com>
2018-11-22 00:43:06 +0300

    Tabs -> Spaces

--- source/app.d
+++ source/app.d
@@ -8,48 +8,48 @@ import asdf;
 
 struct Request
 {
-	@serializationScoped
-	{
-		@serializationFlexible
-		string id;
-		string first_name, last_name;
-	}
+    @serializationScoped
+    {
+        @serializationFlexible
+        string id;
+        string first_name, last_name;
+    }
 }
 
 string date(string format, time_t time)
 {
-	import core.stdc.time : strftime, localtime;
-	import std.string : toStringz;
+    import core.stdc.time : strftime, localtime;
+    import std.string : toStringz;
 
-	char[256] buf;
-	auto size = strftime(buf.ptr, buf.sizeof, format.toStringz(), localtime(&time));
-	return buf[0..size].idup;
+    char[256] buf;
+    auto size = strftime(buf.ptr, buf.sizeof, format.toStringz(), localtime(&time));
+    return buf[0..size].idup;
 }
 
 void handler(scope HTTPServerRequest req, scope HTTPServerResponse res)
 {
-	import std.digest.md;
-	import std.datetime.systime;
-	import std.format : format;
+    import std.digest.md;
+    import std.datetime.systime;
+    import std.format : format;
 
-	auto data = req.bodyReader
-		.readAllUTF8()
-		.deserialize!Request;
+    auto data = req.bodyReader
+        .readAllUTF8()
+        .deserialize!Request;
 
-	res.headers.addField("Content-Type", "application/json");
+    res.headers.addField("Content-Type", "application/json");
 
-	res.writeBody(`{"id":%s,"first_name":"%s","last_name":"%s","current_time":"%s","say":"Dlang is the best"}`
-		.format(
-			data.id,
-			"%s %s".format(data.first_name, data.first_name.md5Of.toHexString()),
-			"%s %s".format(data.last_name, data.last_name.md5Of.toHexString()),
-			date("%F %T %z", Clock.currTime.toUnixTime())
-		)
-	);
+    res.writeBody(`{"id":%s,"first_name":"%s","last_name":"%s","current_time":"%s","say":"Dlang is the best"}`
+        .format(
+            data.id,
+            "%s %s".format(data.first_name, data.first_name.md5Of.toHexString()),
+            "%s %s".format(data.last_name, data.last_name.md5Of.toHexString()),
+            date("%F %T %z", Clock.currTime.toUnixTime())
+        )
+    );
 }
 
 void main()
 {
-	listenHTTP("0.0.0.0:8080", &handler);
-	runApplication();
+    listenHTTP("0.0.0.0:8080", &handler);
+    runApplication();
 }
